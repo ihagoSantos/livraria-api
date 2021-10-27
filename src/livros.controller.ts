093f5e2d4cf74ed2d960d8c787c33b38c3fdc680
Ihago Santos <ihagog12@gmail.com>
2021-10-27 08:59:13 -0300

    crud inicial

---
+++ src/livros.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Livro } from './livro.model';
+import { LivrosService } from "./livros.service";
+
+@Controller('livros')
+export class LivrosController {
+
+    constructor(private livrosService: LivrosService) { } // injeção de dependência
+
+    @Get()
+    obterTodos(): Livro[] {
+        return this.livrosService.obterTodos();
+    }
+
+    @Get(':id')
+    obterUm(@Param() params): Livro {
+        return this.livrosService.obterUm(params.id);
+    }
+
+    @Post()
+    criar(@Body() livro: Livro) {
+        livro.id = 100
+        return this.livrosService.criar(livro);
+    }
+
+    @Put()
+    alterar(@Body() livro: Livro): Livro {
+        return this.livrosService.alterar(livro);
+    }
+
+    @Delete(':id')
+    apagar(@Param() params) {
+        this.livrosService.apagar(params.id);
+    }
+
+}
