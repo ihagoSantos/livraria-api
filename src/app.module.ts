2eed20d8d282773ad7b2e762a7a9b210d4876e43
Ihago Santos <ihagog12@gmail.com>
2021-10-29 12:26:07 -0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,12 +9,13 @@ import { LivrosService } from './livros.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
       SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: 'localhost',
+      host: process.env.HOST_BANCO_DADOS,
       port: 3306,
-      username: 'root',
-      password: 'root',
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
       database: 'livraria',
       autoLoadModels: true, //reconhece automaticamente os modelos criados no projeto
       synchronize: true, //importa automaticamente os módulos e sincroniza com o bd (cria a tabala caso não exista)
